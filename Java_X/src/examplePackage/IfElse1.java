3904bd1bc060037365f93e4161fc91fb3be8f1cf
vivek <vivek.sirsath1@gmail.com>
2023-12-24 23:33:02 +0530

    if condition with true keyword works - ifElse program added

---
+++ Java_X/src/examplePackage/IfElse1.java
@@ -0,0 +1,22 @@
+package examplePackage;
+
+public class IfElse1 {
+
+	public static void main(String[] args) {
+
+		int a = 2;
+		int b = 2;
+		int addition = 4;
+		int subtraction = 0;
+		
+		if((addition==4) == true) {
+			System.out.println("if condition with true keyword works");
+		}
+		
+		if((subtraction==0)) {
+			System.out.println("if condition without true keyword works");
+		}
+
+	}
+
+}
