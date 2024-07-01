ae137eb395793d7ae9c1609ed39b18b09b3d7d29
vivek <vivek.sirsath1@gmail.com>
2024-07-01 20:18:48 +0530

    Ternary Operator code added

---
+++ Java_X/src/examplePackage/TernaryOperator.java
@@ -0,0 +1,16 @@
+package examplePackage;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+
+		// var = (expression) ? Result_01 : Result_02
+
+		int age = 30;
+
+		String res = (age > 18) ? "Eligible" : "Not Eligible";
+		System.out.println(res);
+
+	}
+
+}
