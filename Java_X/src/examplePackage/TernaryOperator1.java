d55bd11f466948bd60120e0e35ae65c35b6a207b
vivek <vivek.sirsath1@gmail.com>
2025-01-18 00:55:12 +0530

    TernaryOperator1.java added

---
+++ Java_X/src/examplePackage/TernaryOperator1.java
@@ -0,0 +1,15 @@
+package examplePackage;
+
+public class TernaryOperator1 {
+
+	public static void main(String[] args) {
+
+        // Ternary Operator / Conditional Operator
+		// Print 0 odd else print 1
+		
+		int num = 15;
+        System.out.println((num % 2 == 1) ? 0 : 1);
+
+	}
+
+}
