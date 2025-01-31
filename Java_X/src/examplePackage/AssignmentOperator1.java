063db4a34d756c00b7c9c8997018114d4527b8d9
vivek <vivek.sirsath1@gmail.com>
2025-01-31 23:40:32 +0530

    AssignmentOperator1 added

---
+++ Java_X/src/examplePackage/AssignmentOperator1.java
@@ -0,0 +1,23 @@
+package examplePackage;
+
+public class AssignmentOperator1 {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		a += 9;
+		System.out.println(a); // 10+9=19
+
+		a -= 5;
+		System.out.println(a); // 19-5=14
+
+		a /= 2;
+		System.out.println(a); // a= a/2 // 14/2=7
+
+		a *= 10;
+		System.out.println(a); // 7*10=70
+
+	}
+
+}
