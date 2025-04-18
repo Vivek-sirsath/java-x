9be3307da4e67d050efac7ddf0a55d7b34dc5414
vivek <vivek.sirsath1@gmail.com>
2025-04-18 22:57:35 +0530

    AssignmentOperator comment adjusted

--- Java_X/src/examplePackage/ComparisonOperator.java
+++ Java_X/src/examplePackage/ComparisonOperator.java
@@ -6,14 +6,15 @@ public class ComparisonOperator {
 		
 		int requiredAge = 18; 
 		
-		System.out.println(requiredAge > 18); // comparison Operator also called as relational operator
-		System.out.println(requiredAge >= 18);
+		// comparison Operator also called as relational operator
+		System.out.println(requiredAge > 18);  // false
+		System.out.println(requiredAge >= 18); // true
 		
 		int num1 = 5;
 		int num2 = 5;
 		
-		System.out.println("Result is:- " + (num1 == num2));
-		System.out.println("If used '!=' then result is:- " + (num1 != num2));
+		System.out.println("Result is:- " + (num1 == num2)); // true
+		System.out.println("If used '!=' then result is:- " + (num1 != num2)); // false
 
 	}
 
